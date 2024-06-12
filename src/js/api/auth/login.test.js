2629f9f4755f83999396f02a317b7797dbaaf44f
Niklas Ferling <niklas.ferling@gmail.com>
2024-06-12 11:14:56 +0200

    Changed test file so search for lower case since original file should not be edited

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -1,33 +1,40 @@
-import { login } from './login';
-import { save } from '../../storage/index';
+import { login } from "./login";
+import { save } from "../../storage/index";
 
 global.fetch = jest.fn(() =>
   Promise.resolve({
     ok: true,
-    json: () => Promise.resolve({ accessToken: 'mockToken', username: 'testUser' }),
+    json: () =>
+      Promise.resolve({ accessToken: "mockToken", username: "testUser" }),
   })
 );
 
-jest.mock('../../storage/index', () => ({
+jest.mock("../../storage/index", () => ({
   save: jest.fn(),
 }));
 
-describe('login function', () => {
-  it('stores a token when provided with valid credentials', async () => {
-    const email = 'test@example.com';
-    const password = 'password';
+describe("login function", () => {
+  it("stores a token when provided with valid credentials", async () => {
+    const email = "test@example.com";
+    const password = "password";
 
     await login(email, password);
 
-    expect(fetch).toHaveBeenCalledWith(expect.stringContaining('/social/auth/login'), {
-      method: 'POST',
-      body: JSON.stringify({ email, password }),
-      headers: {
-        "Content-Type": "application/json",
+    expect(fetch).toHaveBeenCalledWith(
+      expect.stringContaining("/social/auth/login"),
+      {
+        method: "post",
+        body: JSON.stringify({ email, password }),
+        headers: {
+          "Content-Type": "application/json",
+        },
       }
-    });
+    );
 
-    expect(save).toHaveBeenCalledWith('token', 'mockToken');
-    expect(save).toHaveBeenCalledWith('profile', { accessToken: 'mockToken', username: 'testUser' });
+    expect(save).toHaveBeenCalledWith("token", "mockToken");
+    expect(save).toHaveBeenCalledWith("profile", {
+      accessToken: "mockToken",
+      username: "testUser",
+    });
   });
 });
